e12b47990bb6e622a5ca5ca55814f5113a5106b4
Dimar Ilham Tamara <dimartamara1@gmail.com>
2026-07-15 16:13:08 +0700

    Time: 0 ms (100.00%) | Memory: 20.1 MB (66.34%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,48 +1,23 @@
 class Solution {
 public:
-    // dimar's solution
     int maxProfit(vector<int>& prices) {
-        vector<int> lembahBukit, td;
-        int n = prices.size(), hasil = 0;
+        int n = prices.size();
+        int res = 0;
+        for (int x = 0; x < n; x++) {
+            int left = (x == 0) ? INT_MAX : prices[x - 1];
+            int right = (x == n - 1) ? -INT_MAX : prices[x + 1];
+            int curr = prices[x];
+            // lembah
+            if (curr <= left && curr < right) {
+                res -= curr;
+            }
 
-        td.push_back(prices[0]);
-        for (int x = 1; x < n; x++){
-            if (prices[x] != prices[x - 1]){
-                td.push_back(prices[x]);
+            // bukit
+            if (curr > left && curr >= right) {
+                res += curr;
             }
         }
 
-        if (td.size() < 2){
-            return 0;
-        }
-
-        if (td[0] < td[1]){
-            lembahBukit.push_back(td[0]);
-        }
-
-        for (int x = 1; x < td.size() - 1; x++){
-            if ((td[x] < td[x - 1] && td[x] < td[x + 1]) ||
-                 (td[x] > td[x - 1] && td[x] > td[x + 1])){
-                    lembahBukit.push_back(td[x]);
-            }
-        }   
-
-        if (td[td.size() - 1] > td[td.size() - 2]){
-            lembahBukit.push_back(td[td.size() - 1]);
-        }
-
-        for (int x = 0; x < lembahBukit.size(); x++){
-            cout << lembahBukit[x] << " ";
-        }
-
-        if (lembahBukit.size() == 0){
-            return 0;
-        }
-
-        for (int x = 0; x < lembahBukit.size() - 1; x += 2){
-            hasil += (lembahBukit[x + 1] - lembahBukit[x]);
-        }
-
-        return hasil;
+        return res;
     }
 };
