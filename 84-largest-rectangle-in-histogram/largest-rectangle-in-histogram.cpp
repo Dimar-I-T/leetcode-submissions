42552a1399e03708bf56daae709a33369de6d6fa
Dimar Ilham Tamara <dimartamara1@gmail.com>
2026-07-30 13:43:18 +0700

    Time: 437 ms (5.00%) | Memory: 234.8 MB (5.03%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,123 @@
+class Solution {
+public:
+    vector<vector<int>> buildSparseTable(vector<int>& arr) {
+        int n = arr.size();
+        vector<vector<int>> lookup(n + 1, vector<int>(log2(n) + 1));
+
+        for (int i = 0; i < n; i++)
+            lookup[i][0] = arr[i];
+
+        for (int j = 1; (1 << j) <= n; j++) {
+            for (int i = 0; (i + (1 << j) - 1) < n; i++) {
+                if (lookup[i][j - 1] < lookup[i + (1 << (j - 1))][j - 1])
+                    lookup[i][j] = lookup[i][j - 1];
+                else
+                    lookup[i][j] = lookup[i + (1 << (j - 1))][j - 1];
+            }
+        }
+
+        return lookup;
+    }
+
+    int query(int L, int R, vector<vector<int>>& lookup) {
+        int j = (int)log2(R - L + 1);
+        if (lookup[L][j] <= lookup[R - (1 << j) + 1][j])
+            return lookup[L][j];
+        else
+            return lookup[R - (1 << j) + 1][j];
+    }
+
+    int res = 0;
+    unordered_map<int, vector<int>> idx;
+    vector<vector<int>> lookup;
+
+    void f(int l, int r, vector<int>& arr) {
+        //cout << "\n";
+        if (l > r) {
+            return;
+        }
+
+        if (l == r) {
+            res = max(res, arr[l]);
+            return;
+        }
+
+        vector<pair<int, int>> nextLR;
+        int prevIdx = l;
+        int minVal = query(l, r, lookup);
+        //cout << "awal: " << l << " <-> " << r << "\n";
+        //cout << "min: " << minVal << "\n";
+        vector<int> idxMin = idx[minVal];
+        int n = idxMin.size();
+        int i = 0;
+        //cout << "mulai dari kiri:\n";
+        res = max(res, minVal * (r - l + 1));
+        //cout << "luas: " << res << "\n";
+        while (i < n) {
+            int curr = idxMin[i];
+            //cout << "curr: " << curr << "\n";
+            if (curr == prevIdx) {
+                //cout << "sama!\n";
+                break;
+            }
+
+            if (curr > prevIdx) {
+                //cout << "lebih besar!\n";
+                nextLR.push_back({prevIdx, curr - 1});
+                prevIdx = curr;
+                //cout << "sekarang prevIdx: " << prevIdx << "\n";
+                break;
+            }
+
+            i++;
+        }
+
+        int edge = r;
+        i++;
+        //cout << "dari tengah:\n";
+        for (; i < n; i++) {
+            int curr = idxMin[i];
+            //cout << "curr: " << curr << "\n";
+            if (curr == r) {
+                edge = r - 1;
+                break;
+            }
+
+            if (curr > r) {
+                break;
+            }
+
+            if (prevIdx + 1 <= curr - 1) {
+                //cout << "push: " << prevIdx + 1 << " dan " << curr - 1 << "\n";
+                nextLR.push_back({prevIdx + 1, curr - 1});
+            }
+
+            prevIdx = curr;
+        }
+
+        nextLR.push_back({prevIdx + 1, edge});
+
+        //cout << "HASIL: next:\n";
+        for (pair<int, int> p : nextLR) {
+            //cout << "l: " << p.first << " r: " << p.second << "\n";
+        }
+
+        //cout << "MULAI MASUK:\n";
+        for (pair<int, int> p : nextLR) {
+            f(p.first, p.second, arr);
+        }
+    }
+
+    int largestRectangleArea(vector<int>& heights) {
+        lookup = buildSparseTable(heights);
+        int n = heights.size();
+        idx.reserve(n);
+        for (int x = 0; x < n; x++) {
+            int curr = heights[x];
+            idx[curr].push_back(x);
+        }
+
+        f(0, n - 1, heights);
+        return res;
+    }
+};
