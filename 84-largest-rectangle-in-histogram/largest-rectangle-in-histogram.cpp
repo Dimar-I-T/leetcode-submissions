90c99a8aaeed6e99c8f553ef4eb0c044022f71a0
Dimar Ilham Tamara <dimartamara1@gmail.com>
2026-07-31 10:46:33 +0700

    Time: 59 ms (5.50%) | Memory: 87.7 MB (13.65%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,104 +1,95 @@
 class Solution {
 public:
-    vector<vector<int>> buildSparseTable(vector<int>& arr) {
-        int n = arr.size();
-        vector<vector<int>> lookup(n + 1, vector<int>(log2(n) + 1));
+    struct NumIdx {
+        int num;
+        int idx;
+    };
 
-        for (int i = 0; i < n; i++)
-            lookup[i][0] = arr[i];
+    void solveLeftDist(vector<int>& dist, vector<int>& v) {
+        int n = v.size();
+        stack<NumIdx> st;
+        st.push({-INT_MAX, -1});
+        for (int x = 0; x < n; x++) {
+            NumIdx prev = st.top();
+            int prevNum = prev.num;
+            
+            int currNum = v[x];
+            int currIdx = x;
+            if (prevNum >= currNum) {
+                // ke kiri
+                while (!st.empty()) {
+                    prev = st.top();
+                    int currLeftNum = prev.num;
+                    int currLeftIdx = prev.idx;
+                    if (currLeftIdx >= 0 && dist[currLeftIdx] == 0) {
+                        dist[currLeftIdx] = 1;
+                    }
 
-        for (int j = 1; (1 << j) <= n; j++) {
-            for (int i = 0; (i + (1 << j) - 1) < n; i++) {
-                if (lookup[i][j - 1] < lookup[i + (1 << (j - 1))][j - 1])
-                    lookup[i][j] = lookup[i][j - 1];
-                else
-                    lookup[i][j] = lookup[i + (1 << (j - 1))][j - 1];
+                    if (currLeftNum < currNum) {
+                        dist[currIdx] = currIdx - currLeftIdx;
+                        break;
+                    }
+
+                    st.pop();
+                }
             }
-        }
 
-        return lookup;
+            st.push({currNum, currIdx});
+        }
     }
 
-    int query(int L, int R, vector<vector<int>>& lookup) {
-        int j = (int)log2(R - L + 1);
-        if (lookup[L][j] <= lookup[R - (1 << j) + 1][j])
-            return lookup[L][j];
-        else
-            return lookup[R - (1 << j) + 1][j];
-    }
+    void solveRightDist(vector<int>& dist, vector<int>& v) {
+        int n = v.size();
+        stack<NumIdx> st;
+        st.push({-INT_MAX, -1});
+        for (int x = 0; x < n; x++) {
+            NumIdx prev = st.top();
+            int prevNum = prev.num;
 
-    int res = 0;
-    unordered_map<int, vector<int>> idx;
-    vector<vector<int>> lookup;
+            int currNum = v[x];
+            int currIdx = x;
+            if (prevNum >= currNum) {
+                // KE KIRI
+                while (!st.empty()) {
+                    prev = st.top();
+                    int currLeftNum = prev.num;
+                    int currLeftIdx = prev.idx;
+                    if (currLeftIdx >= 0) {
+                        dist[currLeftIdx] = currIdx - currLeftIdx;
+                    }
 
-    void f(int l, int r, vector<int>& arr) {
-        if (l > r) {
-            return;
-        }
+                    if (currLeftNum < currNum) {
+                        break;
+                    }
 
-        if (l == r) {
-            res = max(res, arr[l]);
-            return;
-        }
-
-        vector<pair<int, int>> nextLR;
-        int prevIdx = l;
-        int minVal = query(l, r, lookup);
-        vector<int> idxMin = idx[minVal];
-        int n = idxMin.size();
-        int i = 0;
-        res = max(res, minVal * (r - l + 1));
-        while (i < n) {
-            int curr = idxMin[i];
-            if (curr == prevIdx) {
-                break;
+                    st.pop();
+                }
             }
 
-            if (curr > prevIdx) {
-                nextLR.push_back({prevIdx, curr - 1});
-                prevIdx = curr;
-                break;
-            }
-
-            i++;
-        }
-
-        int edge = r;
-        i++;
-        for (; i < n; i++) {
-            int curr = idxMin[i];
-            if (curr == r) {
-                edge = r - 1;
-                break;
-            }
-
-            if (curr > r) {
-                break;
-            }
-
-            if (prevIdx + 1 <= curr - 1) {
-                nextLR.push_back({prevIdx + 1, curr - 1});
-            }
-
-            prevIdx = curr;
-        }
-
-        nextLR.push_back({prevIdx + 1, edge});
-        for (pair<int, int> p : nextLR) {
-            f(p.first, p.second, arr);
+            st.push({currNum, currIdx});
         }
     }
 
     int largestRectangleArea(vector<int>& heights) {
-        lookup = buildSparseTable(heights);
+        heights.push_back(-INT_MAX);
         int n = heights.size();
-        idx.reserve(n);
-        for (int x = 0; x < n; x++) {
-            int curr = heights[x];
-            idx[curr].push_back(x);
+        vector<int> leftDist(n), rightDist(n);
+
+        // distKiri (prev smaller)
+        solveLeftDist(leftDist, heights);
+
+        // distKanan (next smaller)
+        solveRightDist(rightDist, heights);
+
+        int res = 0;
+        for (int x = 0; x < n - 1; x++) {
+            int left = leftDist[x];
+            int right = rightDist[x];
+            int val = heights[x];
+            int area = val * (right + left - 1);
+            res = max(res, area);
         }
 
-        f(0, n - 1, heights);
         return res;
     }
 };
