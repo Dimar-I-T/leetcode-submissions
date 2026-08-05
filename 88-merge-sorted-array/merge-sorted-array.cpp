4faaef37f993807728e92ee6ec64ade319bea375
Dimar Ilham Tamara <dimartamara1@gmail.com>
2026-08-05 13:11:44 +0700

    Time: 0 ms (100.00%) | Memory: 12.8 MB (10.32%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -2,54 +2,36 @@ class Solution {
 public:
     // dimar's solution
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> hasil;
-        queue<int> q1, q2;
-        for (int x = 0; x < n; x++){
-            q2.push(nums2[x]);
+        vector<int> container(n + m);
+        int l1 = 0, l2 = 0, k = 0;
+        while (l1 < m && l2 < n) {
+            int n1 = nums1[l1];
+            int n2 = nums2[l2];
+            if (n1 <= n2) {
+                container[k] = n1;
+                l1++;
+            } else {
+                container[k] = n2;
+                l2++;
+            }
+
+            k++;
         }
 
-        for (int x = 0; x < m; x++){
-            q1.push(nums1[x]);
-        }
-    
-        int kiri1 = 0, kiri2 = 0;
-        while (!q1.empty() || !q2.empty()){
-            if (q1.empty()){
-                break;
-            }
-            
-            if (q2.empty()){
-                break;
-            }
-            if (q1.front() <= q2.front()){
-                hasil.push_back(q1.front());
-                q1.pop();
-                kiri1++;
-            }else{
-                hasil.push_back(q2.front());
-                q2.pop();
-                kiri2++;
-            }
+        while (l1 < m) {
+            container[k] = nums1[l1];
+            l1++;
+            k++;
         }
 
-        if (q1.empty()){
-            for (int x = kiri2; x < n; x++){
-                hasil.push_back(nums2[x]);
-            }
-        }
-        
-        if (q2.empty()){
-            for (int x = kiri1; x < m; x++){
-                hasil.push_back(nums1[x]);
-            }
+        while (l2 < n) {
+            container[k] = nums2[l2];
+            l2++;
+            k++; 
         }
 
-        for (int x = 0; x < m; x++){
-            nums1[x] = hasil[x];
-        }
-
-        for (int x = m; x < n + m; x++){
-            nums1[x] = hasil[x];
+        for (int x = 0; x < k; x++) {
+            nums1[x] = container[x];
         }
     }
 };
