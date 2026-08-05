dbb168384881cd2751851e348239bc2144de9550
Dimar Ilham Tamara <dimartamara1@gmail.com>
2026-08-05 13:00:48 +0700

    Time: 4 ms (32.07%) | Memory: 8.6 MB (31.24%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -2,32 +2,24 @@ class Solution {
 public:
     // dimar's solution
     string mergeAlternately(string word1, string word2) {
-        int minn = 0;
-        string sisa = "";
-        if (word1.length() < word2.length()){
-            minn = word1.length();
-            sisa = word2;
-        }else{
-            minn = word2.length();
-            sisa = word1;
-        }
-
-        string hasil = "";
-        int w1 = 0, w2 = 0;
-        for (int x = 0; x < minn*2; x++){
-            if (x % 2 == 0){
-                hasil += word1[w1];
-                w1++;
-            }else{
-                hasil += word2[w2];
-                w2++;
+        int n1 = word1.length(), n2 = word2.length();
+        int i1 = 0, i2 = 0;
+        int k = 0;
+        string res = "";
+        while (i1 < n1 || i2 < n2) {
+            if (i1 < n1 && k % 2 == 0) {
+                res += word1[i1];
+                i1++;
             }
-        }
-        
-        if (w1 <= sisa.length() - 1){
-            hasil += sisa.substr(w1, sisa.length() - w1);
+
+            if (i2 < n2 && k % 2 == 1) {
+                res += word2[i2];
+                i2++;
+            }
+
+            k++;
         }
 
-        return hasil;
+        return res;
     }
 };
