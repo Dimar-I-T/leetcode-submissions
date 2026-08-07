324774fdbe27c3ab96e8c3b204b074978f28aa41
Dimar Ilham Tamara <dimartamara1@gmail.com>
2026-08-07 09:58:46 +0700

    Time: 0 ms (100.00%) | Memory: 15.2 MB (36.90%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -8,12 +8,7 @@ public:
         while (l <= r) {
             int mid = (l + r) / 2;
             int curr = nums[mid];
-            //cout << "l: " << l << "\n";
-            //cout << "r: " << r << "\n";
-            //cout << "mid: " << mid << "\n";
-            //cout << "curr vs target = " << curr << " vs " << target << "\n";
             if (curr == target) {
-                //cout << "SAMA!\n";
                 return mid;
             }
 
@@ -22,18 +17,6 @@ public:
             bool bLeft2 = (target <= mostLeft) && (target <= curr) && (mostLeft <= curr);
             bool bLeft3 = (mostLeft <= curr) && (mostLeft <= target) && (curr <= target);
             if (bLeft1 || bLeft2 || bLeft3) {
-                if (bLeft1) {
-                    //cout << "KE KANAN: (curr < target) && (curr < mostLeft) && (target < mostLeft)\n\n";
-                }
-
-                if (bLeft2) {
-                    //cout << "KE KANAN: (target < mostLeft) && (target < curr) && (mostLeft < curr)\n\n";
-                }
-
-                if (bLeft3) {
-                    //cout << "KE KANAN: (mostLeft < curr) && (mostLeft < target) && (curr < target)\n\n";
-                }
-
                 l = mid + 1;
                 continue;
             }
@@ -43,18 +26,6 @@ public:
             bool bRight2 = (curr <= mostLeft) && (curr <= target) && (mostLeft <= target);
             bool bRight3 = (mostLeft <= target) && (mostLeft <= curr) && (target <= curr);
             if (bRight1 || bRight2 || bRight3) {
-                if (bRight1) {
-                    //cout << "KE KIRI: (target < curr) && (target < mostLeft) && (curr < mostLeft)\n\n";
-                }
-
-                if (bRight2) {
-                    //cout << "KE KIRI: (curr < mostLeft) && (curr < target) && (mostLeft < target)\n\n";
-                }
-
-                if (bRight3) {
-                    //cout << "KE KIRI: (mostLeft < target) && (mostLeft < curr) && (target < curr)\n\n";
-                }
-
                 r = mid - 1;
                 continue;
             }
