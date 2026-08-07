298fd50622316a374eceb0524a95dedf0e20ca8c
Dimar Ilham Tamara <dimartamara1@gmail.com>
2026-08-07 14:47:17 +0700

    Time: 53 ms (13.98%) | Memory: 33 MB (9.68%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -10,39 +10,27 @@ public:
         um.reserve(n);
         um[s[l]] = -1;
         for (int r = 1; r < n; r++) {
-            //cout << "l: " << l << "\n";
-            //cout << "prevC: " << s[l] << "\n";
             char c = s[r];
-            //cout << "c: " << c << "\n";
             // kasus edge
             auto itr = um.find(c);
             bool isDuplicate = 0;
             if (itr != um.end()) {
                 int p = itr->second;
                 if (l == 0 && p == -1) {
-                    //cout << "ADA DUPLIKAT DI P: " << p << "\n\n";
                     um[s[l]] = 0;
                     l++;
                 }
 
                 // ada duplikat
                 if (p > 0) {
-                    //cout << "ADA DUPLIKAT DI P: " << p << "\n";
                     while (l <= p) {
                         um[s[l]] = 0;
-                        //cout << "CLEAR " << s[l] << "\n";
                         l++;
                     }
-                    
-                    //cout <<"\n";
                 }
             }
             
-            //cout << "MASUKKAN: " << r - l + 1 << "\n\n";
             res = max(res, r - l + 1);
-
-            //cout << "AMAN";
-            //cout << "\n\n";
             um[c] = r;
         }
 
