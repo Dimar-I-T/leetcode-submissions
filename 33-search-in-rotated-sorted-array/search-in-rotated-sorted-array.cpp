99dc6abca4640f892a37dd6e0dd1d9ff95378e2c
Dimar Ilham Tamara <dimartamara1@gmail.com>
2026-08-07 09:58:19 +0700

    Time: 0 ms (100.00%) | Memory: 15.3 MB (36.90%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,65 +1,65 @@
 class Solution {
 public:
-    // dimar's solution
     int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1, hasil = -1;
-        while (l <= r){
-            int m = (l + r)/2;
-            if (target == nums[r]){
-                hasil = r;
-                break;
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        int res = INT_MAX;
+        int mostLeft = nums[n - 1];
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            int curr = nums[mid];
+            //cout << "l: " << l << "\n";
+            //cout << "r: " << r << "\n";
+            //cout << "mid: " << mid << "\n";
+            //cout << "curr vs target = " << curr << " vs " << target << "\n";
+            if (curr == target) {
+                //cout << "SAMA!\n";
+                return mid;
             }
 
-            if (l == m){
-                if (target == nums[m]){
-                    hasil = m;
+            // l to mid
+            bool bLeft1 = (curr <= target) && (curr <= mostLeft) && (target <= mostLeft);
+            bool bLeft2 = (target <= mostLeft) && (target <= curr) && (mostLeft <= curr);
+            bool bLeft3 = (mostLeft <= curr) && (mostLeft <= target) && (curr <= target);
+            if (bLeft1 || bLeft2 || bLeft3) {
+                if (bLeft1) {
+                    //cout << "KE KANAN: (curr < target) && (curr < mostLeft) && (target < mostLeft)\n\n";
                 }
 
-                break;
+                if (bLeft2) {
+                    //cout << "KE KANAN: (target < mostLeft) && (target < curr) && (mostLeft < curr)\n\n";
+                }
+
+                if (bLeft3) {
+                    //cout << "KE KANAN: (mostLeft < curr) && (mostLeft < target) && (curr < target)\n\n";
+                }
+
+                l = mid + 1;
+                continue;
             }
 
-            cout << m << " ";
-            if (target < nums[l]){
-                if (target == nums[m]){
-                    hasil = m;
-                    break;
-                }else{
-                    if (nums[m] < nums[l]){
-                        if (target > nums[m]){
-                            l = m;
-                        }else{
-                            r = m;
-                        }
-                    }else{
-                        if (target < nums[m]){
-                            l = m;
-                        }else{
-                            r = m;
-                        }
-                    }
+            // r to mid
+            bool bRight1 = (target <= curr) && (target <= mostLeft) && (curr <= mostLeft);
+            bool bRight2 = (curr <= mostLeft) && (curr <= target) && (mostLeft <= target);
+            bool bRight3 = (mostLeft <= target) && (mostLeft <= curr) && (target <= curr);
+            if (bRight1 || bRight2 || bRight3) {
+                if (bRight1) {
+                    //cout << "KE KIRI: (target < curr) && (target < mostLeft) && (curr < mostLeft)\n\n";
                 }
-            }else{
-                if (target == nums[m]){
-                    hasil = m;
-                    break;
-                }else{
-                    if (nums[m] < nums[l]){
-                        if (target > nums[m]){
-                            r = m;
-                        }else{
-                            l = m;
-                        }
-                    }else{
-                        if (target > nums[m]){
-                            l = m;
-                        }else{
-                            r = m;
-                        }
-                    }
+
+                if (bRight2) {
+                    //cout << "KE KIRI: (curr < mostLeft) && (curr < target) && (mostLeft < target)\n\n";
                 }
+
+                if (bRight3) {
+                    //cout << "KE KIRI: (mostLeft < target) && (mostLeft < curr) && (target < curr)\n\n";
+                }
+
+                r = mid - 1;
+                continue;
             }
         }
 
-        return hasil;
+        return -1;
     }
 };
