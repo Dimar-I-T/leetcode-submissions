eeb18b5bf673686e408ed2c083fcfbb15c15d634
Dimar Ilham Tamara <dimartamara1@gmail.com>
2026-08-07 14:46:34 +0700

    Time: 51 ms (14.43%) | Memory: 33.1 MB (9.68%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,28 +2,50 @@ class Solution {
 public:
 // dimar's solution
     int lengthOfLongestSubstring(string s) {
-        int n = s.length(), hasil = 1;
-        int l = 0, banyak = 1;
-        int lok[128] = {0};
-        memset(lok, -1, sizeof(lok));
-        lok[int(s[0])] = 0; 
-        for (int x = 1; x < n; x++){
-            if (lok[s[x]] >= l){
-                hasil = max(hasil, banyak);
-                banyak -= lok[int(s[x])] - l + 1;
-                l = lok[int(s[x])] + 1;
-                lok[int(s[x])] = -1;
+        int n = s.length();
+        int l = 0;
+        int res = (n == 0) ? 0 : 1;
+        // char -> position
+        unordered_map<char, int> um;
+        um.reserve(n);
+        um[s[l]] = -1;
+        for (int r = 1; r < n; r++) {
+            //cout << "l: " << l << "\n";
+            //cout << "prevC: " << s[l] << "\n";
+            char c = s[r];
+            //cout << "c: " << c << "\n";
+            // kasus edge
+            auto itr = um.find(c);
+            bool isDuplicate = 0;
+            if (itr != um.end()) {
+                int p = itr->second;
+                if (l == 0 && p == -1) {
+                    //cout << "ADA DUPLIKAT DI P: " << p << "\n\n";
+                    um[s[l]] = 0;
+                    l++;
+                }
+
+                // ada duplikat
+                if (p > 0) {
+                    //cout << "ADA DUPLIKAT DI P: " << p << "\n";
+                    while (l <= p) {
+                        um[s[l]] = 0;
+                        //cout << "CLEAR " << s[l] << "\n";
+                        l++;
+                    }
+                    
+                    //cout <<"\n";
+                }
             }
+            
+            //cout << "MASUKKAN: " << r - l + 1 << "\n\n";
+            res = max(res, r - l + 1);
 
-            lok[s[x]] = x;
-            banyak++;
+            //cout << "AMAN";
+            //cout << "\n\n";
+            um[c] = r;
         }
 
-        hasil = max(hasil, banyak);
-        if (n == 0){
-            hasil = 0;
-        }
-
-        return hasil;
+        return res;
     }
 };
