14294fb940d2572cdca20e65cb2c796a010b2314
Dimar Ilham Tamara <dimartamara1@gmail.com>
2026-08-11 16:45:08 +0700

    Time: 7 ms (44.26%) | Memory: 14.9 MB (5.42%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -14,33 +14,19 @@ public:
         Maks maks = {s[l], freq[s[l]]};
         int res = 1;
         while (r < n) {
-            //cout << "l: " << l << "\n";
-            //cout << "r: " << r << "\n";
-            //cout << "sr: " << s[r] << "\n";
             freq[s[r]]++;
-            //cout << "freq sr sekarang: " << freq[s[r]] << "\n";
-            //cout << "maks.c = " << maks.c << "\n";
-            //cout << "maks.freq = " << maks.freq << "\n";
             if (freq[s[r]] > maks.freq) {
-                //cout << "KANAN LEBIH BESAR FREQ, GANTI\n";
                 maks = {s[r], freq[s[r]]};
-                //cout << "maks.c sekarang: " << s[r] << "\n";
-                //cout << "maks.freq sekarang: " << maks.freq << "\n";
             }
 
-            //cout << "minRep = " << r - l + 1 - maks.freq << "\n";
-            //cout << "k: " << k << "\n";
             if (r - l + 1 - maks.freq > k) {
-                //cout << "MIN REP LEBIH BESAR, GESER L\n\n";
                 if (s[l] == maks.c) {
-                    //cout << "SL itu sama dengan maks, kurangi freq\n\n";
                     maks.freq--;
                 }
 
                 freq[s[l]]--;
                 l++;
             } else {
-                //cout << "MINREP AMAN, SIMPAN RES: res atau " << r - l + 1 << "\n\n";
                 res = max(res, r - l + 1);
             }
 
