a0c34cb949f6813a77a5c11a466d3c02d1ba2c82
Dimar Ilham Tamara <dimartamara1@gmail.com>
2026-09-02 20:55:24 +0700

    Time: 3 ms (25.95%) | Memory: 12.7 MB (5.80%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,48 +1,28 @@
 class Solution {
 public:
-    // dimar's solution
-    set<vector<int>> hasil;
-    void permutasi(vector<int> kiri, vector<int> kanan) {
-        int n = kanan.size();
-        int m = kiri.size();
-        if (n == 0) {
+    void backtrack(int i, vector<int> sek, vector<int>& nums, vector<vector<int>>& hasil, vector<bool> visited) {
+        int n = nums.size();
+        if (sek.size() == n) {
+            hasil.push_back(sek);
             return;
         }
 
-        kiri.push_back(0);
-        vector<int> kiri1 = kiri;
         for (int x = 0; x < n; x++) {
-            kiri1[m] = kanan[x];
-            vector<int> kanan1;
-            for (int y = 0; y < n; y++) {
-                if (y == x) {
-                    continue;
-                }
-
-                kanan1.push_back(kanan[y]);
+            if (visited[x]) {
+                continue;
             }
 
-            vector<int> perm;
-            for (int y : kiri1) {
-                perm.push_back(y);
-            }
-
-            for (int y : kanan1) {
-                perm.push_back(y);
-            }
-
-            hasil.insert(perm);
-            permutasi(kiri1, kanan1);
+            sek.push_back(nums[x]);
+            visited[x] = 1;
+            backtrack(x, sek, nums, hasil, visited);
+            visited[x] = 0;
+            sek.pop_back();
         }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
-        permutasi({}, nums);
-        vector<vector<int>> hasilV;
-        for (vector<int> v : hasil) {
-            hasilV.push_back(v);
-        }
-
-        return hasilV;
+        vector<vector<int>> hasil;
+        backtrack(0, {}, nums, hasil, vector<bool>(nums.size()));
+        return hasil;
     }
 };
